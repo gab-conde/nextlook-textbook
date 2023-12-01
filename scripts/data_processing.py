5da7bb9d527a8e9679987e38298ce6e365f228b6
Gabriella Conde <conde.gabriella.r@gmail.com>
2023-12-01 06:16:51 -0500

    Add processing script

---
+++ scripts/data_processing.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def process_book_data():
+
+    # Convert the csv to a dataframe
+    book_data = pd.read_csv('datasets/BNTextbook_2022-02-05.csv')
+
+    # Filter the dataframe to exclude non-vital columns
+    book_columns = ['title', 'edition', 'publisher', 'ISBN']
+    book_data = book_data[book_columns]
+
+    # Remove rows with missing data
+    book_data.dropna(axis='index', how='any', inplace=True)
+
+    # Remove duplicate textbooks from the dataframe
+    book_data.drop_duplicates(subset='title', keep='first', inplace=True)
+    book_data.drop_duplicates(subset='ISBN', keep='first', inplace=True)
