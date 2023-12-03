a56cc4d69ce8a883986afa6ee72f84500396356b
Gabriella Conde <conde.gabriella.r@gmail.com>
2023-12-03 06:56:38 -0500

    Add book class

---
+++ models/book.py
@@ -0,0 +1,9 @@
+# Book resource
+class Book:
+
+    # Constructor
+    def __init__(self, title, edition, publisher, ISBN) -> None:
+        self.title = title
+        self.editiion = edition
+        self.publisher = publisher
+        self.ISBN = ISBN
